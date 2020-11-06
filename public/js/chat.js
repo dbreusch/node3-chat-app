5d08227d26c4ea26698e8704e0e7e2f83e2be276
David Reusch <dbreusch@me.com>
2020-11-06 11:25:25 -0800

    Add first client and server events

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,14 @@
-io()
+// client-side code for socket.io functionality
+
+const socket = io()
+
+// receive event from server
+socket.on('countUpdated', (count) => {
+    console.log('The count has been updated', count)
+})
+
+// send event to server when button is clcked
+document.querySelector('#increment').addEventListener('click', () => {
+    console.log('Clicked')
+    socket.emit('increment')
+})
