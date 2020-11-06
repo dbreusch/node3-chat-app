129d3ba13985721f95b9f8cabb2595341647b0f8
David Reusch <dbreusch@me.com>
2020-11-06 12:18:39 -0800

    Add form and related event handlers

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,13 +2,32 @@
 
 const socket = io()
 
-// receive event from server
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated', count)
+const messageForm = document.querySelector('#message-form')
+// const messageText = document.querySelector('#message')
+
+socket.on('message', (message) => {
+    console.log(message)
 })
 
-// send event to server when button is clcked
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('Clicked')
-    socket.emit('increment')
+// send event to server when form button is clcked
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()  // stop form refresh after submit
+
+    // console.log('Form submitted')
+
+    // const message = messageText.value
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message)
 })
+
+// -- count button app
+// // receive event from server
+// socket.on('countUpdated', (count) => {
+//     console.log('The count has been updated', count)
+// })
+//
+// // send event to server when button is clcked
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log('Clicked')
+//     socket.emit('increment')
+// })
--- src/index.js
+++ src/index.js
@@ -12,22 +12,31 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
+// -- count button app
+// let count = 0
 
 // server (emit) -> client (receive) - countUpdated
 // client (emit) -> server (receive) - increment
 
-// server function for client connections
+// handle client connections
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('countUpdated', count)
+    socket.emit('message', 'Welcome!')
 
-    socket.on('increment', () => {
-        count++
-        // socket.emit('countUpdated', count)   // send to specific connection
-        io.emit('countUpdated', count)          // send to ALL connections
+    socket.on('sendMessage', (message) => {
+        console.log(message)
+        io.emit('message', message)          // send to ALL connections
     })
+
+    // -- count button app
+    // socket.emit('countUpdated', count)
+    //
+    // socket.on('increment', () => {
+    //     count++
+    //     // socket.emit('countUpdated', count)   // send to specific connection
+    //     io.emit('countUpdated', count)          // send to ALL connections
+    // })
 })
 
 // start express server on port
